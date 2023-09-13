cb384585a8a742908795d6f3d893ab228ba40bd1
onesuper <onesuperclark@gmail.com>
2023-09-13 15:24:38 +0800

    Fix hardcode endpoint

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo '$XINFERENCE_SERVER_ENDPOINT is: ' ${XINFERENCE_SERVER_ENDPOINT}
+
 # Launch Llama 2 Chat model 
 xinference launch --model-name "llama-2-chat" --model-format "ggmlv3" --size-in-billions 13 --endpoint ${XINFERENCE_SERVER_ENDPOINT}
 
@@ -7,10 +9,10 @@ xinference launch --model-name "llama-2-chat" --model-format "ggmlv3" --size-in-
 xinference launch --model-name "gte-base" --model-type "embedding" --endpoint ${XINFERENCE_SERVER_ENDPOINT}
 
 # Capture the model UID from output
-export XINFERENCE_LLM_MODEL_UID=$(python3 -c "from xinference.client import Client; c = Client('http://0.0.0.0:8001'); data=c.list_models(); print(list(data.keys())[0])")
+export XINFERENCE_LLM_MODEL_UID=$(python3 -c "from xinference.client import Client; c = Client('$XINFERENCE_SERVER_ENDPOINT'); data=c.list_models(); print(list(data.keys())[0])")
 
 # Capture the model UID from output
-export XINFERENCE_EMBEDDING_MODEL_UID=$(python3 -c "from xinference.client import Client; c = Client('http://0.0.0.0:8001'); data=c.list_models(); print(list(data.keys())[1])")
+export XINFERENCE_EMBEDDING_MODEL_UID=$(python3 -c "from xinference.client import Client; c = Client('$XINFERENCE_SERVER_ENDPOINT'); data=c.list_models(); print(list(data.keys())[1])")
 
 # Pass the Model UID via an environment variable
 echo "XINFERENCE_EMBEDDING_MODEL_UID is: ${XINFERENCE_EMBEDDING_MODEL_UID}"
